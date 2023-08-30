0dc8596841db335f0b04ce0d2a1b7bc03c7992b1
AnshD8619 <ansh.desai86@gmail.com>
2023-08-29 19:50:25 -0600

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BTAnshDesai.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BTAnshDesai.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BTUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/BTUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BTAnshDesai.Models
+{
+    public class BTUser: IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Sur Name")]
+        public string? SurName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string? FullName { get { return $"{FirstName} {SurName}"; } }
+
+       
+
+    }
+}
